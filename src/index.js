a25a3cb6df3a410e55c7b6a338a3e8449962370a
Mayank Jain <jainmayank16041999@gmail.com>
2020-08-22 22:11:56 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+require('dotenv').config()
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+
 
 const app = express()
 const port = process.env.PORT
